0f7fb6c521cfb56b9a3d5865cabc410084edc5ea
Shashank Shridhar <173191559+Shashank-006@users.noreply.github.com>
2025-06-15 22:36:19 -0400

    Finish grid layout

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const squareLength = 16;
 
 const boxContainer = document.createElement("div");
 boxContainer.classList.toggle("boxContainer");
-const box = document.createElement("div");
-boxContainer.appendChild(box);
 
-box.classList.toggle("box");
+for(let i = 0; i < squareLength; i++) {
+    const row = document.createElement("div");
+    row.classList.toggle("row");
+    for(let j = 0; j < squareLength; j++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square");
+        row.appendChild(square);
+    }
+    boxContainer.appendChild(row);
+}
 
 document.body.appendChild(boxContainer);
