2ac3c2d59a63b35a5cd98262520332958b1b0dc5
Shashank Shridhar <173191559+Shashank-006@users.noreply.github.com>
2025-06-15 23:44:56 -0400

    Finish button to change size

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const squareLength = 16;
 
 const boxContainer = document.createElement("div");
 boxContainer.classList.toggle("boxContainer");
+const sizeButton = document.querySelector(".size-button");
 
-for(let i = 0; i < squareLength; i++) {
-    const row = document.createElement("div");
-    row.classList.toggle("row");
-    for(let j = 0; j < squareLength; j++) {
-        const square = document.createElement("div");
-        square.classList.toggle("square");
-        row.appendChild(square);
+function buildSquares(length) {
+    for(let i = 0; i < length; i++) {
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+        for(let j = 0; j < length; j++) {
+            const square = document.createElement("div");
+            square.classList.toggle("square");
+            row.appendChild(square);
+        }
+        boxContainer.appendChild(row);
     }
-    boxContainer.appendChild(row);
+}
+
+function removeSquares() {
+    const rows = document.querySelectorAll(".boxContainer > *");
+    rows.forEach(r => boxContainer.removeChild(r));
 }
 
 boxContainer.addEventListener("mouseover", (e) => {
@@ -19,6 +27,20 @@ boxContainer.addEventListener("mouseover", (e) => {
     if(square.classList.contains("square")) {
         square.setAttribute("style", "background-color: blue");
     }
-})
+});
+
+sizeButton.addEventListener("click", () => {
+    let result;
+    do {
+        result = prompt("What size do you want to pick? (1 - 100)");
+        result = +result;
+    }
+    while (Number.isNaN(result) || !Number.isInteger(result) || result < 1 || result > 100);
+    
+    removeSquares();
+    buildSquares(result);
+});
+
+buildSquares(squareLength);
 
 document.body.appendChild(boxContainer);
